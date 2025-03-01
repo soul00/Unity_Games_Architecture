eeddd6121e74a16ac257c69a03076b35a1f46ca8
Pasha <soul00555@gmail.com>
2025-03-01 15:13:16 -0400

    Initial Project upload

---
+++ Assets/Infrastructure/GameBootstrapper.cs
@@ -0,0 +1,17 @@
+using System;
+using UnityEngine;
+
+namespace Infrastructure
+{
+    public class GameBootstrapper : MonoBehaviour
+    {
+        private Game _game;
+
+        private void Awake()
+        {
+            _game = new Game();
+            
+            DontDestroyOnLoad(this);
+        }
+    }
+}
---
+++ Assets/Services/Input/InputService.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+namespace Services.Input
+{
+    public abstract class InputService : IInputService
+    {
+        protected const string Horizontal = "Horizontal";
+        protected const string Vertical = "Vertical";
+        private const string Button = "Fire";
+
+        public abstract Vector2 Axis { get; }
+
+        public bool IsAttackButtonUp() => SimpleInput.GetButtonUp(Button);
+    }
+
+    class StandaloneInputService : InputService
+    {
+        public override Vector2 Axis
+        {
+            get
+            {
+                Vector2 axis = SimpleInputAxis();
+
+                if (axis == Vector2.zero)
+                    axis = UnityAxis();
+                
+                return axis;
+            }
+        }
+
+        private static Vector2 SimpleInputAxis() =>
+            new Vector2(SimpleInput.GetAxis(Horizontal), SimpleInput.GetAxis(Vertical));
+
+        private static Vector2 UnityAxis() =>
+            new Vector2(UnityEngine.Input.GetAxis(Horizontal), UnityEngine.Input.GetAxis(Horizontal));
+    }
+}
