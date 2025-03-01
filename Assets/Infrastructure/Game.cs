9f1505967aaa6b6d002f58e6456335a7edc08d3d
Pasha <soul00555@gmail.com>
2025-03-01 15:53:44 -0400

    Created implementation for 2 InputServices

--- Assets/Infrastructure/Game.cs
+++ Assets/Infrastructure/Game.cs
@@ -1,7 +1,23 @@
-﻿namespace Infrastructure
+﻿using Services.Input;
+using UnityEngine;
+
+namespace Infrastructure
 {
     public class Game
     {
-        
+        public static IInputService InputService;
+
+        public Game()
+        {
+            ChooseInputService();
+        }
+
+        private static void ChooseInputService()
+        {
+            if (Application.isEditor)
+                InputService = new StandaloneInputService();
+            else
+                InputService = new MobileInput();
+        }
     }
 }
---
+++ Assets/Services/Input/MobileInput.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+namespace Services.Input
+{
+  class MobileInput : InputService
+  {
+    public override Vector2 Axis => SimpleInputAxis();
+  }
+}
